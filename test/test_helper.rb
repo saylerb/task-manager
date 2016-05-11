5f6a793b44a3d9c56c5a780be7c3e923726a21f7
saylerb <sayler.b@gmail.com>
2016-05-11 15:16:37 -0600

    add Capybara to test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require "minitest/autorun"
 require "minitest/emoji"
 require "tilt/erb"
+require "capybara"
 
 module TestHelpers
   def teardown
@@ -15,4 +16,10 @@ module TestHelpers
     database = YAML::Store.new('db/task_manager_test')
     @task_manager ||= TaskManager.new(database)
   end
+end
+
+Capybara.app = TaskManagerApp
+
+class FeatureTest < Minitst::Test
+  include Capybara::DSL
 end
